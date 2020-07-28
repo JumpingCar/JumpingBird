6c66b46caca0bf1680406f9837681acd05bc10d3
pacokwon <haechank@gmail.com>
2020-07-29 00:31:21 +0900

    add: include gene editor in app, prevent space scroll

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react'
+import { useEffect } from 'react'
 import Sketch from './Sketch'
-
+import GeneEditor from './GeneEditor'
+import './app.css'
 
 const App: React.FC = () => {
-    
+    useEffect(() => {
+        window.addEventListener('keydown', e => {
+            if (e.keyCode === 32 && e.target === document.body)
+                e.preventDefault()
+        })
+    }, [])
 
     return (
         <div>
             <Sketch />
+            <GeneEditor />
         </div>
     )
 }
